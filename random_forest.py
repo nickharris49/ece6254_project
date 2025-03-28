0de0d74e718f043f70a9c900da003f40d150b67d
saikaryekar <saikaryekar0712@gmail.com>
2025-03-28 01:00:54 -0400

    updated rf and lr

--- random_forest.py
+++ random_forest.py
@@ -117,13 +117,13 @@ def train_and_evaluate_models(X_train, X_val, X_test, y_train, y_val, y_test):
         metrics = {
             "MSE": mean_squared_error(y_val, y_pred_val),
             "MAE": mean_absolute_error(y_val, y_pred_val),
-            "R2 Score": r2_score(y_val, y_pred_val)
+            "R2 Score": r2_score(y_val, y_pred_val),
         }
 
         test_metrics = {
             "MSE": mean_squared_error(y_test, y_pred_test),
             "MAE": mean_absolute_error(y_test, y_pred_test),
-            "R2 Score": r2_score(y_test, y_pred_test)
+            "R2 Score": r2_score(y_test, y_pred_test),
         }
 
         results[model_name] = {"Validation Metrics": metrics, "Test Metrics": test_metrics}
@@ -138,8 +138,8 @@ def train_and_evaluate_models(X_train, X_val, X_test, y_train, y_val, y_test):
 def main():
     datadir = "./DATASET/"
 
-    X = np.load(datadir + "feature_vector_full.npy")
-    y = np.load(datadir + "y.npy").reshape(-1)
+    X = np.load(datadir + "feature_vector_full_normalized.npy")
+    y = np.load(datadir + "y_normalized.npy").reshape(-1)
 
     X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.2, random_state=42)
     X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.4, random_state=42)
