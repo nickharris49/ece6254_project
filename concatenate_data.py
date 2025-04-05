bf2a9d88d45212ccdb230e992682953d4b470bbe
Nicholas Harris <nharris49@gatech.edu>
2025-04-05 16:55:57 -0400

    un-normalized subject-wise dataset

--- concatenate_data.py
+++ concatenate_data.py
@@ -28,12 +28,12 @@ def main():
         x100k = []
         data = np.load(full_path + data_file)
         for window in data:
-            y.append((window['knee_angle_l'] - np.mean(window['knee_angle_l'])) / np.std(window['knee_angle_l']))
-            r5k.append((window['knee_bioz_5k_resistance'] - np.mean(window['knee_bioz_5k_resistance'])) / np.std(window['knee_bioz_5k_resistance']))
-            x5k.append((window['knee_bioz_5k_reactance'] - np.mean(window['knee_bioz_5k_reactance'])) / np.std(window['knee_bioz_5k_reactance']))
-            r100k.append((window['knee_bioz_100k_resistance'] - np.mean(window['knee_bioz_100k_resistance'])) / np.std(window['knee_bioz_100k_resistance']))
-            x100k.append((window['knee_bioz_100k_reactance'] - np.mean(window['knee_bioz_100k_reactance'])) / np.std(window['knee_bioz_100k_reactance']))
-
+            y.append(window['knee_angle_l'])
+            r5k.append(window['knee_bioz_5k_resistance'])
+            x5k.append(window['knee_bioz_5k_reactance'])
+            r100k.append(window['knee_bioz_100k_resistance'])
+            x100k.append(window['knee_bioz_100k_reactance'])
+            
         y = np.expand_dims(np.concatenate(y, axis=0), axis=1)
         r5k = np.expand_dims(np.concatenate(r5k, axis=0), axis=1)
         x5k = np.expand_dims(np.concatenate(x5k, axis=0), axis=1)
@@ -44,10 +44,10 @@ def main():
         feat_vec_100k = np.concatenate((r100k, x100k), axis=1)
         feat_vec_full = np.concatenate((r5k, x5k, r100k, x100k), axis=1)
 
-        np.save(full_path + "feature_vector_5k_normalized_" + subj_num + ".npy", feat_vec_5k)
-        np.save(full_path + "feature_vector_100k_normalized_" + subj_num + ".npy", feat_vec_100k)
-        np.save(full_path + "feature_vector_full_normalized_" + subj_num + ".npy", feat_vec_full)
-        np.save(full_path + "y_normalized_" + subj_num + ".npy", y)
+        np.save(full_path + "feature_vector_5k_" + subj_num + ".npy", feat_vec_5k)
+        np.save(full_path + "feature_vector_100k_" + subj_num + ".npy", feat_vec_100k)
+        np.save(full_path + "feature_vector_full_" + subj_num + ".npy", feat_vec_full)
+        np.save(full_path + "y_" + subj_num + ".npy", y)
 
 if __name__ == '__main__':
     main()
